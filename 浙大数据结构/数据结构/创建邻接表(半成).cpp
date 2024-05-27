9db1e3fe490799cc7d7c29b5187f5f171cc995b7
to-dim-stars <2548443986@qq.com>
2024-05-27 17:14:38 +0800

    vsvode-git-test

--- 浙大数据结构/数据结构/创建邻接表(半成).cpp
+++ 浙大数据结构/数据结构/创建邻接表(半成).cpp
@@ -1,13 +1,20 @@
-#include<iotream>
-
+#include<iostream>
+#include<string>
 using namespace std;
 
-#define VexsMax 100
-
-struct GamPlayer{
-	int uid=1000001;
-	char name[7]="mihoyo";
+class GamePlayer {
+	public:
+	static int uid = 1000000;//static使每个Player的uid不同 
+	string name = "mihoyo";//string 初始化问题 
+	//各函数默认
+	void ScanGamePlayer(){
+		cin>>name;
+		uid++;
+	}
 };
+
+
+#define VexsMax 100
 
 typedef struct ArcNode{  //Arc弧，adj "adjacent"相邻的
 	int adjvex;     			//弧指向的尾顶点在vexs的下标 
@@ -16,18 +23,69 @@ typedef struct ArcNode{  //Arc弧，adj "adjacent"相邻的
 }ArcNode;
 
 typedef struct VNode{  //V顶点 
-	GamePalyer* data=NULL;	//数据的指针 
+	GamePlayer data;	//数据的指针 
 	ArcNode *firstarc=NULL;  //顶点指向的第一条边 
 }VNode,Vexs[VexsMax];
 
-struct ALGragh{
-	Vexs//这会是指针吗 
+struct ALGraph{		 
+	Vexs arr;		//依书上说,这会是指针吗 
+	int vexsnum=0;	//相关数量
+	int arcnodenum=0; //相关数量
 };
+
+
+int localvex(ALGraph &G,string p){
+	int i,num=G.vexsnum;
+	for(i=0;i<num;i++){
+		if(G.arr[i].data.name==p)return i;
+	}return -1;
+}
+
+bool CreateALGraph(ALGraph &G) {
+	int i;
+	if (!G.vexsnum) {
+		cout << "请完善图信息...\n图总节点数：";
+		cin >> G.vexsnum;
+		cout << "\n图弧\\边总数：";
+		cin >> G.arcnodenum;
+	}
+	int num = G.vexsnum;
+	for (i = 0; i < num; i++) {
+		printf("uid:%d的角色名(6位内): ",G.arr[i].data.uid);
+		G.arr[i].data.ScanGamePlayer();
+	}
+	//节点数组完成
+	num=G.arcnodenum;
+	string p1,p2;
+	int l1,l2;
+	for(i = 0; i < num; i++){
+		cout<<"边:" ;
+		cin>>p1>>p2;
+		cout<<'\n';
+		l1=localvex(G,p1);
+		l2=localvex(G,p2);
+		if(l1!=-1&&l2!=-1){	//若有向图则一次便可 
+			ArcNode* a=new ArcNode;
+			a->adjvex=l2;
+			a->nextarc=G.arr[l1].firstarc;
+			G.arr[l1].firstarc=a;
+		}
+		
+		
+	}
+	return true;
+}
+
+int PrintALGraph(const ALGraph& G){
+	
+}
 
 
 int main (){
 	
-	
+	ALGraph f1;
+	CreateALGraph(f1);
+	PrintALGraph(f1);
 	
 	return 0;
 }
---
+++ 算法/归并ai.cpp
@@ -0,0 +1,156 @@
+
+#include <stdio.h>
+
+
+
+// 合并两个已排序的数组
+
+void merge(int arr[], int left[], int leftSize, int right[], int rightSize, int merged[]) {
+
+	int leftIndex = 0, rightIndex = 0, mergedIndex = 0;
+
+
+
+	// 合并两个子数组
+
+	while (leftIndex < leftSize && rightIndex < rightSize) {
+
+		if (left[leftIndex] <= right[rightIndex]) {
+
+			merged[mergedIndex++] = left[leftIndex++];
+
+		} else {
+
+			merged[mergedIndex++] = right[rightIndex++];
+
+		}
+
+	}
+
+
+
+	// 复制剩余的左侧数组元素
+
+	while (leftIndex < leftSize) {
+
+		merged[mergedIndex++] = left[leftIndex++];
+
+	}
+
+
+
+	// 复制剩余的右侧数组元素
+
+	while (rightIndex < rightSize) {
+
+		merged[mergedIndex++] = right[rightIndex++];
+
+	}
+
+}
+
+
+
+// 归并排序函数
+
+void mergeSort(int arr[], int size) {
+
+	if (size <= 1) {
+
+		// 数组大小为0或1时，已经是排序好的
+
+		return;
+
+	}
+
+
+
+	int mid = size / 2;
+
+
+
+	// 临时数组
+
+	int left[mid];
+
+	int right[size - mid];
+
+
+
+	// 复制左侧子数组
+
+	for (int i = 0; i < mid; i++) {
+
+		left[i] = arr[i];
+
+	}
+
+
+
+	// 复制右侧子数组
+
+	for (int i = mid; i < size; i++) {
+
+		right[i - mid] = arr[i];
+
+	}
+
+
+
+	// 递归排序左右子数组
+
+	mergeSort(left, mid);
+
+	mergeSort(right, size - mid);
+
+
+
+	// 合并两个已排序的子数组
+
+	int merged[size];
+
+	merge(arr, left, mid, right, size - mid, merged);
+
+
+
+	// 将合并后的数组复制回原数组
+
+	for (int i = 0; i < size; i++) {
+
+		arr[i] = merged[i];
+
+	}
+
+}
+
+
+
+int main() {
+
+	int arr[] = {38, 27, 43, 3, 9, 82, 10};
+
+	int size = sizeof(arr) / sizeof(arr[0]);
+
+
+
+	// 排序数组
+
+	mergeSort(arr, size);
+
+
+
+	// 输出排序后的数组
+
+	for (int i = 0; i < size; i++) {
+
+		printf("%d ", arr[i]);
+
+	}
+
+	printf("\n");
+
+
+
+	return 0;
+
+}
